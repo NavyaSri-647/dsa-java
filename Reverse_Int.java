b487e8719041001a8ebdfcc81607bbc9802fccd4
Navya Sri Ravulacheruvu <navyasri.rc@gmail.com>
2026-07-10 20:38:40 +0530

    Create Reverse_Int.java

---
+++ Reverse_Int.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Reverse_Int{
+	public static int reverse_integer(int x){
+		int rev = 0;
+		while(x!=0){
+			int rem = x % 10;
+			
+			if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && rem < -8)){
+				return 0;
+			}
+			rev = rev*10 + rem;
+			x = x/10;
+		}
+		return rev;
+	}
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter an integer: ");
+		
+		int n = sc.nextInt();
+		int reverse = reverse_integer(n);
+		
+		System.out.print("Reverse integer of " + n + " is " +reverse);
+	}
+}
