8fc368100c6453d514dc76abd4b86dfcc9c6d959
Navya Sri Ravulacheruvu <navyasri.rc@gmail.com>
2026-08-11 10:55:56 +0530

    Create Reverse_String.java

---
+++ Strings/Reverse_String.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Reverse_String{
+    public static String reverseString(String s){
+        char[] arr = s.toCharArray();
+        int l = 0;
+        int r = arr.length-1;
+        while(l < r){
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+        return new String(arr);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String res = reverseString(s);
+        System.out.println(res);
+        sc.close();
+    }
+}
